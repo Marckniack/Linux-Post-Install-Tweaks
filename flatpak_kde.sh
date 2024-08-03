6a7ebed36ecf19a07a98ec9273e742329fc9be95
Marckniack <Marckniack96@hotmail.com>
2024-08-03 15:33:52 +0200

    Create flatpak_kde.sh

---
+++ flatpak_kde.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### ALL
+flatpak install --assumeyes --noninteractive io.github.spacingbat3.webcord org.telegram.desktop org.gimp.GIMP org.kde.krita org.blender.Blender org.inkscape.Inkscape com.spotify.Client org.mozilla.firefox
+
+### KDE
+flatpak install --assumeyes --noninteractive org.qbittorrent.qBittorrent org.kde.okular org.kde.gwenview io.github.f3d_app.f3d
+
+### GAMING
+flatpak install --assumeyes --noninteractive io.github.lime3ds.Lime3DS com.valvesoftware.Steam com.heroicgameslauncher.hgl org.ryujinx.Ryujinx
