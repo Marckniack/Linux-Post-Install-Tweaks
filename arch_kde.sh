4dc85f570e1212dd8af45608a5859437191472bd
Marckniack <Marckniack96@hotmail.com>
2024-08-05 08:40:10 +0200

    Update arch_kde.sh

--- arch_kde.sh
+++ arch_kde.sh
@@ -25,7 +25,7 @@ ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0302
 
 # Enable runtime PM for NVIDIA VGA/3D controller devices on adding device
 ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"' >> /etc/udev/rules.d/80-nvidia-pm.rules
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"' | sudo tee /etc/udev/rules.d/80-nvidia-pm.rules
 
 
 # Enable nvidia services for suspension
