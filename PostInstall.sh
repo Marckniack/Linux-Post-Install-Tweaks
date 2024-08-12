f27dcf3eeae9b979135df299b371f17d87cb5f44
Marckniack <Marckniack96@hotmail.com>
2024-08-12 18:24:03 +0200

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -110,26 +110,37 @@ function setup_gnome()
 #setup_common
 
 # Run Menu
-PS3='Select Options: '
-options=("Setup KDE" "Setup GNOME" "Quit")
-select opt in "${options[@]}"
-do
-  case $opt in
-      "Setup KDE")
-	  setup_common
-	  setup_kde
-	  break
-	  ;;
-      "Setup GNOME")
-	  setup_common
-	  setup_gnome
-	  break
-	  ;;
-      "Quit")
-	  break
-	  ;;
-      *) echo "invalid option $REPLY";;
-  esac
-done
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Backtitle here"
+TITLE="Title here"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "KDE"
+         2 "GNOME"
+         #3 "Option 3"
+	 )
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "You chose Option 1"
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+esac
 
 
