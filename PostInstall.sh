f329c1cea75a6f5553584b377baf92e76e98dde8
Marckniack <Marckniack96@hotmail.com>
2024-08-13 05:34:49 +0200

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -114,6 +114,40 @@ gnome()
 	pacman -Rcns epiphany evince gnome-system-monitor loupe tracker3-miners network-manager-applet --noconfirm || exit 1
 }
 
+kde_flatpak()
+{
+	### ALL
+	flatpak install --assumeyes --noninteractive io.github.spacingbat3.webcord org.telegram.desktop org.gimp.GIMP org.kde.krita org.blender.Blender org.inkscape.Inkscape com.spotify.Client org.mozilla.firefox
+	
+	### KDE
+	flatpak install --assumeyes --noninteractive org.qbittorrent.qBittorrent org.kde.okular org.kde.gwenview io.github.f3d_app.f3d
+	
+	### GAMING
+	flatpak install --assumeyes --noninteractive io.github.lime3ds.Lime3DS com.valvesoftware.Steam com.heroicgameslauncher.hgl org.ryujinx.Ryujinx
+	
+	### UPDATE
+	flatpak update --assumeyes --noninteractive
+}
+
+gnome_flatpak()
+{
+	### ALL
+	flatpak install --assumeyes --noninteractive io.github.spacingbat3.webcord org.telegram.desktop org.gimp.GIMP org.kde.krita org.blender.Blender org.inkscape.Inkscape com.spotify.Client org.mozilla.firefox
+	
+	### GAMING
+	flatpak install --assumeyes --noninteractive io.github.lime3ds.Lime3DS com.valvesoftware.Steam com.heroicgameslauncher.hgl org.ryujinx.Ryujinx com.vysp3r.ProtonPlus
+	
+	### GNOME
+	flatpak install --assumeyes --noninteractive ca.desrt.dconf-editor page.codeberg.libre_menu_editor.LibreMenuEditor com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager io.missioncenter.MissionCenter 
+	flatpak install --assumeyes --noninteractive org.gnome.TextEditor io.github.nokse22.Exhibit com.usebottles.bottles io.github.amit9838.mousam org.gnome.Papers org.gnome.World.PikaBackup org.gnome.Calendar 
+	flatpak install --assumeyes --noninteractive io.github.celluloid_player.Celluloid com.transmissionbt.Transmission org.gnome.Loupe
+	flatpak install --assumeyes --noninteractive org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+ 
+	### UPDATE
+	flatpak update --assumeyes --noninteractive
+}
+
+
 
 # Setup common packages and settings
 #setup_common
