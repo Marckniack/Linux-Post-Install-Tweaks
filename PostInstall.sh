17bd8898c65f23ebbedc4ec0f9777c6b09c73a94
Marckniack <Marckniack96@hotmail.com>
2024-08-15 21:59:03 +0200

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -55,7 +55,7 @@ ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0302
 	systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-persistenced.service nvidia-resume.service
 	
 	########## ADDITIONAL PACKAGES ##########
-	pacman -Syu noto-fonts noto-fonts-cjk noto-fonts-emoji flatpak flatpak-xdg-utils power-profiles-daemon papirus-icon-theme ttf-dejavu fwupd gamemode lib32-gamemode --noconfirm || exit 1
+	pacman -Syu noto-fonts noto-fonts-cjk noto-fonts-emoji flatpak flatpak-xdg-utils power-profiles-daemon papirus-icon-theme ttf-dejavu fwupd gamemode lib32-gamemode system-config-printer --noconfirm || exit 1
 	pacman -Syu ttf-droid distrobox podman pacman-contrib git curl wget bash-completion android-tools android-udev ntfs-3g nano acpid acpi acpi_call p7zip unarchiver unrar --noconfirm || exit 1 
 
 	########### SERVICES ############
@@ -176,6 +176,31 @@ gnome_flatpak()
  
 	### UPDATE
 	flatpak update --assumeyes --noninteractive
+}
+
+base_post()
+{
+	########### INSTALL YAY ###########
+	git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+ 	########### SET NVIDIA ###########
+	# sudo nano /etc/mkinitcpio.conf
+	# MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)
+	# remove kms from "HOOKS" in /etc/mkinitcpio.conf
+	# sudo mkinitcpio -p linux
+
+ 	########### BOOT FLAGS ###########
+	# sudo nano /boot/loader/entries/arch.conf
+	# quiet loglevel=2 systemd.show_status=no console=ttyS0,115200 console=tty0 vt.global_cursor_default=0
+
+	###########  XPADNEO ###########
+	git clone https://github.com/atar-axis/xpadneo.git && cd xpadneo && sudo ./install.sh
+	
+ 	### USE .LOCAL/BIN FOLDER ###
+ 	echo -e 'export PATH="$PATH:$HOME/.local/bin/"' | tee -a ~/.bash_profile
+
+  	########### SpotX-Bash (Spotify Adblock) ###########
+	bash <(curl -sSL https://spotx-official.github.io/run.sh)
 }
 
 
