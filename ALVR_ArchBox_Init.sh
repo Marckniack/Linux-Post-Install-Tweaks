6e9a117bce09dc98acc2883802328d1d82277269
Marckniack <Marckniack96@hotmail.com>
2024-08-25 08:04:29 +0200

    Update ALVR_ArchBox_Init.sh

--- ALVR_ArchBox_Init.sh
+++ ALVR_ArchBox_Init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ### Create Box
-# distrobox create --pull --image docker.io/archlinux/archlinux:latest --name arch --home /home/marco/.local/share/containers/boxes/arch/ --nvidia --pre-init-hooks "pacman -Syy reflector --noconfirm && reflector --country 'Canada,United States,France,Germany,Australia' --latest 5 --sort rate --save /etc/pacman.d/mirrorlist"
+# distrobox create --pull --image quay.io/toolbx/arch-toolbox:latest --name arch --home /home/marco/.local/share/containers/boxes/arch/ --nvidia --pre-init-hooks "pacman -Syy reflector --noconfirm && reflector --country 'Canada,United States,France,Germany,Australia' --latest 5 --sort rate --save /etc/pacman.d/mirrorlist"
 
 # Renaming xdg-open from container because it will run host applications (like steam) instead of internal ones
-sudo mv /usr/local/bin/xdg-open /usr/local/bin/xdg-open2
+# sudo mv /usr/local/bin/xdg-open /usr/local/bin/xdg-open2
 
 # Setting up arch
 echo "[multilib]" | sudo tee -a /etc/pacman.conf
@@ -14,17 +14,22 @@ sudo pacman-key --init || exit 1
 echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
 echo "LC_ALL=en_US.UTF-8" | sudo tee /etc/locale.conf
 
-sudo pacman -q -Syu glibc lib32-glibc xdg-utils at-spi2-core lib32-at-spi2-core tar wget --noconfirm || exit 1
-# sudo pacman -q qt5-tools qt5-multimedia  --noconfirm || exit 1
-sudo pacman -q -Syu git vim base-devel noto-fonts xdg-user-dirs fuse libx264 sdl2 libva-utils xorg-server nano ffmpeg --noconfirm || exit 1
-sudo pacman -q -Syu nvidia-prime --noconfirm --assume-installed nvidia-utils || exit 1
-sudo pacman -q -Syu libva-mesa-driver vulkan-intel lib32-vulkan-intel intel-media-driver lib32-libva-intel-driver libva-intel-driver --noconfirm || exit 1
-sudo pacman -q -Syu lib32-pipewire pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber --noconfirm || exit 1
-sudo pacman -q -Syu steam --noconfirm --assume-installed vulkan-driver --assume-installed lib32-vulkan-driver || exit 1
+sudo pacman -q -Sy glibc lib32-glibc xdg-utils at-spi2-core lib32-at-spi2-core tar wget --noconfirm || exit 1
+sudo pacman -q -Sy qt5-tools qt5-multimedia  --noconfirm || exit 1
+sudo pacman -q -Sy git vim base-devel noto-fonts xdg-user-dirs fuse libx264 sdl2 libva-utils xorg-server nano ffmpeg --noconfirm || exit 1
+sudo pacman -q -Sy nvidia-prime --noconfirm --assume-installed nvidia-utils || exit 1
+sudo pacman -q -Sy libva-mesa-driver vulkan-intel lib32-vulkan-intel intel-media-driver lib32-libva-intel-driver libva-intel-driver --noconfirm || exit 1
+sudo pacman -q -Sy lib32-pipewire pipewire pipewire-pulse pipewire-alsa wireplumber --noconfirm || exit 1
+sudo pacman -q -Sy steam --noconfirm --assume-installed vulkan-driver --assume-installed lib32-vulkan-driver || exit 1
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd || exit 1
+cd || exit 1
+git clone https://aur.archlinux.org/yay.git || exit 1
+cd yay || exit 1
+makepkg -si --noconfirm || exit 1
+cd || exit 1
 
-mkdir ~/.config || exit 1
+sudo rm -rf yay 2>/dev/null
+mkdir ~/.config 2>/dev/null
 
 xdg-mime default steam.desktop x-scheme-handler/steam
 steam steam://install/250820 &>/dev/null &
@@ -36,7 +41,7 @@ sleep 3
 distrobox-host-exec pkexec setcap CAP_SYS_NICE+ep "$HOME/.steam/steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher" || exit 1
 
 # patching steamvr (without it, steamvr might lag to hell)
-sh "$HOME//patch_bindings_spam.sh" || exit 1
+sh "$HOME//patch_bindings_spam.sh" 2>/dev/null
 
 # C# Development
 sudo pacman -q -Syu dotnet-sdk dotnet-host dotnet-runtime --noconfirm || exit 1
